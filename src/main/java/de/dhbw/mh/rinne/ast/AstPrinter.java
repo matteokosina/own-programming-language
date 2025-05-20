2e49dfc9a7d84eb5c4f35b07daf3f9e7094b34be
kilihunter <sturm268@gmail.com>
2025-05-20 16:23:04 +0200

    Update AstPrinter.java

--- src/main/java/de/dhbw/mh/rinne/ast/AstPrinter.java
+++ src/main/java/de/dhbw/mh/rinne/ast/AstPrinter.java
@@ -66,5 +66,12 @@ public class AstPrinter extends AstVisitor<String> {
     // Team 7
 
     // Team 8
+    @Override
+    String visitReturnStmt(AstReturnStmtNode node) {
+        enterNode();
+        String temp = indentationFor(level) + "Return(" + node.locationAsString() + ")\n" + visitChildren(node);
+        exitNode();
+        return temp;
+    }
 
 }
