9cb175b26a388cc590fe58777eae00d18f804109
marco-haupt <132950989+marco-haupt@users.noreply.github.com>
2025-06-01 22:14:43 +0200

    feat: capture and test ANTLR lexer/parser errors
    Replaced default ANTLR error reporting with a custom error listener that captures lexer and parser errors. These are now validated against expected outputs in unit tests to ensure correctness.
    
    This prevents unnoticed grammar errors and improves CI reliability.

--- src/main/java/de/dhbw/mh/rinne/LoggingErrorListener.java
+++ src/main/java/de/dhbw/mh/rinne/LoggingErrorListener.java
@@ -1,23 +1,44 @@
 package de.dhbw.mh.rinne;
 
+import java.util.BitSet;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 
 import de.dhbw.mh.rinne.ast.AstNode;
 
 public final class LoggingErrorListener extends BaseErrorListener implements RinneErrorListener {
 
     private final List<String> messages = new LinkedList<>();
+    private final String prefix;
+
+    public LoggingErrorListener() {
+        this("");
+    }
+
+    public LoggingErrorListener(String prefix) {
+        this.prefix = prefix;
+    }
 
     @Override
     public void receive(AstNode node, String message) {
-        messages.add(message);
+        messages.add(prefix + message);
     }
 
     public List<String> messages() {
         return messages;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
+            String msg, RecognitionException e) {
+        messages.add(prefix + line + ":" + charPositionInLine + ": " + msg);
     }
 
 }
